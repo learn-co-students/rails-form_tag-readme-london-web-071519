12036e8beb7cd0edbc81ba5033d6a4282636c5f3
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-08-12 16:26:27 +0100

    Rails Form Tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index]
+  #index
+  get '/posts', to: 'posts#index', as: 'posts'
+
+  #new
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+
+  #create
+  post '/posts', to: 'posts#create'
 end
